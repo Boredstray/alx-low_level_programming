1c0669bded3add6fe0ab41ba291f29f9f1f3861d
Boredstray <eye.gabriel773@gmail.com>
2022-09-11 15:45:42 -0700

    C_variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,9 @@
  * and print the last digit of the number stored in the variable n
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
