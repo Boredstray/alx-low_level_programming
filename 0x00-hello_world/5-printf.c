fd1adfcdf908f2da2bae4d8acdab3985b904db16
Boredstray <eye.gabriel773@gmail.com>
2022-09-11 14:03:33 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
- *
- * * main - print the string in the put function
- *
- * * Return: 0 if exited properly, non-zero otherwise
- *
+ * main - print the string in the put function
+ * Return: 0 if exited properly, non-zero otherwise
  * */
 
 int main(void)
